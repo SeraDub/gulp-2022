a8c1636bdf64bc0b70eeb2085aea3433f8ccfb90
SeraDub <dubsera604@gmail.com>
2022-01-29 20:56:26 +0300

    y

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,1 +1,15 @@
-const buildFolder ='./dist'
+import path, *as nodePart from 'path';
+const rootFolder = nodePart.basename(nodePart.resolve());
+
+const buildFolder =`./dist`;
+const srcFolder =`./src`;
+
+console path = {
+	builf:{},
+	src:{},
+	watch:{},
+	clean: buildFolder,
+	srcFolder:srcFolder,
+	rootFolder:rootFolder,
+	ftp:``
+}
